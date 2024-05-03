6d5f82c09e7a2f873aad2def767c401d79334bbd
Farhan Afsar <mdfarhanafsar@gmail.com>
2024-05-03 21:17:40 +0600

    First

---
+++ music_player/lib/models/playlist_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'song.dart';
+
+class PlaylistProvider extends ChangeNotifier {
+  final List<Song> _playlist = [
+    Song(
+      songName: "Is Qadr",
+      artistName: "Darshan Raval",
+      albumArtImagePath: "assets/images/img1.jpeg",
+      audioPath: "assets/audio/song1.mp3",
+    ),
+    Song(
+      songName: "Mayabi",
+      artistName: "Ahamed Zaki",
+      albumArtImagePath: "assets/images/img2.jpeg",
+      audioPath: "assets/audio/song2.mp3",
+    ),
+    Song(
+      songName: "Is Qadr",
+      artistName: "Darshan Raval",
+      albumArtImagePath: "assets/images/img3.jpeg",
+      audioPath: "assets/audio/song1.mp3",
+    ),
+  ];
+
+  int? _currentSongIndex;
+
+  List<Song> get playlist => _playlist;
+  int? get currentSongIndex => _currentSongIndex;
+
+  set currentSongIndex(int? newIndex) {
+    _currentSongIndex = newIndex;
+
+    notifyListeners();
+  }
+}
