66f67d3d54b8f579762db04b1e1a98be5b06287e
Farhan Afsar <mdfarhanafsar@gmail.com>
2024-05-03 23:35:03 +0600

    Second

--- music_player/lib/pages/song_page.dart
+++ music_player/lib/pages/song_page.dart
@@ -9,64 +9,142 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-      builder: (context, value, chile) => Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.arrow_back),
-                    ),
-                    Text("P L A Y"),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.menu),
-                    ),
-                  ],
-                ),
-                NeuBox(
+      builder: (context, value, chile) {
+        final playlist = value.playlist;
+        final currentSong = playlist[value.currentSongIndex ?? 0];
+
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: const Icon(Icons.arrow_back),
+                      ),
+                      const Text("P L A Y I N G"),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.menu),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NeuBox(
                     child: Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.asset("assets/images/img1.jpeg"),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(currentSong.albumArtImagePath),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                "Is Qadr",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    currentSong.songName,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(currentSong.artistName),
+                                ],
                               ),
-                              Text("Darshan Raval"),
+                             const Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )
                             ],
                           ),
-                          Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                          )
-                        ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("0:00"),
+                            Icon(Icons.shuffle),
+                            Icon(Icons.repeat),
+                            Text("0:00"),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
-                ))
-              ],
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 0),
+                        ),
+                        child: Slider(
+                          min: 0,
+                          max: 100,
+                          value: 50,
+                          activeColor: Colors.green,
+                          onChanged: (value) {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.skip_previous),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.play_arrow),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(
+                            child: Icon(Icons.skip_next),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
